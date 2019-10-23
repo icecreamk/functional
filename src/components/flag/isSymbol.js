2ee19b69d06c599c89ba68f9627a6124e71e8417
林可欣 <282023@nd.com>
2019-10-23 20:44:48 +0800

    toNumber before

---
+++ src/components/flag/isSymbol.js
@@ -0,0 +1,12 @@
+import {
+    getTag
+} from '../internal/getTag'
+
+function isSymbol(value) {
+    const type = typeof value
+    return type == 'symbol' || (type === 'object' && value != null && getTag(value) == '[object Symbol]')
+}
+
+export {
+    isSymbol
+}
---
+++ src/components/internal/getTag.js
@@ -0,0 +1,11 @@
+const toString = Object.prototype.toString
+
+function getTag(val) {
+    if (val == null) {
+        return val === undefined ? '[object Undefined]' : '[object Null]'
+    }
+    return toString.call(val)
+}
+export {
+    getTag
+} 
